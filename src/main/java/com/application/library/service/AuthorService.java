ff1d75a44bc7191a15da6c93c716590eba17cd11
Alen Boric <boricalen@live.com>
2022-11-04 23:24:29 +0100

    Book,Author-service,repository

---
+++ src/main/java/com/application/library/service/AuthorService.java
@@ -0,0 +1,30 @@
+package com.application.library.service;
+
+import com.application.library.entity.Author;
+import com.application.library.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> findAllAuthors(){
+        return authorRepository.findAll();
+
+    }
+
+    public Author findAuthor(Long id){
+        Author author = authorRepository.findById(id).orElseThrow(() -> new RuntimeException("Author not found"));
+        return author;
+    }
+
+   public void createAuthor(Author author){
+        authorRepository.save(author);
+   }
+
+}
