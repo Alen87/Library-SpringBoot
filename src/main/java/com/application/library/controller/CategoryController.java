3f8a08cd98facc16fa636288d90e2498edb27d7e
Alen Boric <boricalen@live.com>
2022-11-05 18:43:21 +0100

    CategoryController

--- src/main/java/com/application/library/controller/CategoryController.java
+++ src/main/java/com/application/library/controller/CategoryController.java
@@ -6,25 +6,54 @@ import com.application.library.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class CategoryController {
     @Autowired
-   private CategoryService categoryService;
+    private CategoryService categoryService;
 
 
     @GetMapping("/categories")
-    public String findAllCategories(Model model){
+    public String findAllCategories(Model model) {
         List<Category> categories = categoryService.findAllCategories();
-        model.addAttribute("categories",categories);
+        model.addAttribute("categories", categories);
         return "categories";
 
 
     }
 
+    @GetMapping("/remove-category/{id}")
+    public String deleteCategory(@PathVariable Long id, Model model) {
+        categoryService.deleteCategoryById(id);
+        model.addAttribute("categories", categoryService.findAllCategories());
+        return "categories";
+
+    }
+
+    @GetMapping("update-category/{id}")
+    public String updateCategory(@PathVariable Long id,Model model){
+        model.addAttribute("category",categoryService.findCategoryById(id));
+        return "update-category";
+
+
+    }
+
+    @PostMapping("update-category/{id}")
+    public String saveCategory(@PathVariable Long id, Category category, BindingResult bindingResult,Model model){
+        if(bindingResult.hasErrors()){
+            return "update-category";
+        }
+        categoryService.updateCategory(category);
+        model.addAttribute("categories",categoryService.findAllCategories());
+        return "redirect:/categories";
+
+    }
 
 
 }
